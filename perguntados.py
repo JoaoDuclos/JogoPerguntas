fe91ada10cf5baefd2cd40c30c18a579a489ca9a
JoaoDuclos <113364116+JoaoDuclos@users.noreply.github.com>
2024-04-10 10:26:33 -0300

    Add files via upload
    Adicionado categorias e uma função que acessa essas categorias

--- perguntados.py
+++ perguntados.py
@@ -1,19 +1,31 @@
-perguntas = [
-    {'pergunta':'Qual o nome do rei(a) da inglaterra?', 'resposta':'Rei Charles','alternativas':['Rainha Elizabeth', 'Rei Charles', 'Rei Arthur', 'Rei Do Mate']},
-    {'pergunta':'Quanto é 2 + 2', 'resposta':'Rei Charles','alternativas':['Rainha Elizabeth', 'Rei Charles', 'Rei Arthur', 'Rei Do Mate']},
-    {'pergunta':'Qual o nome do rei(a) da inglaterra?', 'resposta':'Rei Charles','alternativas':['Rainha Elizabeth', 'Rei Charles', 'Rei Arthur', 'Rei Do Mate']},
-    {'pergunta':'Qual o nome do rei(a) da inglaterra?', 'resposta':'Rei Charles','alternativas':['Rainha Elizabeth', 'Rei Charles', 'Rei Arthur', 'Rei Do Mate']},
-    {'pergunta':'Qual o nome do rei(a) da inglaterra?', 'resposta':'Rei Charles','alternativas':['Rainha Elizabeth', 'Rei Charles', 'Rei Arthur', 'Rei Do Mate']},
-    {'pergunta':'Qual o nome do rei(a) da inglaterra?', 'resposta':'Rei Charles','alternativas':['Rainha Elizabeth', 'Rei Charles', 'Rei Arthur', 'Rei Do Mate']},
-    {'pergunta':'Qual o nome do rei(a) da inglaterra?', 'resposta':'Rei Charles','alternativas':['Rainha Elizabeth', 'Rei Charles', 'Rei Arthur', 'Rei Do Mate']},
-]
+categories = {
+    'general': [{'pergunta':'Qual o nome do rei(a) da inglaterra?', 'resposta':'Rei Charles','alternativas':['Rainha Elizabeth', 'Rei Charles', 'Rei Arthur', 'Rei Do Mate']}],
+    'history': [{'pergunta':'Quem foi o nome mais famoso da revolução francesa?', 'resposta':'Napoleão','alternativas':['Napoleão', 'Hitler', 'Bejamin', 'Arthur']}],
+    'science': [{'pergunta':'Qual é o elemento mais abundante no universo?', 'resposta':'Hidrogênio','alternativas':['Oxigênio', 'Nitrogênio', 'Carbono', 'Hidrogênio']}],
+    'geography': [{'pergunta':'Qual é o maior país do mundo em área terrestre?', 'resposta':'Rússia','alternativas':['Canadá', 'Estados Unidos', 'China', 'Rússia']}]
+}
 
-for pergunta in perguntas:
-    print(pergunta['pergunta'])
-    for i, auternativa in enumerate(pergunta['alternativas']):
-        print(f'{i+1}) {auternativa}')
-    resposta = int(input('Resposta: '))
-    if pergunta['resposta'] == pergunta['alternativas'][resposta-1]:
-        print('Resposta Certa')
-    else:
-        print('Resposta Errada')
+for i, category in enumerate(categories):
+    print(f'{i+1}) {category}')
+cat = int(input('Qual genero você deseja? '))
+
+def makingTheQuestion(cat):
+    for pergunta in categories[cat]:
+        print(pergunta['pergunta'])
+        for i, auternativa in enumerate(pergunta['alternativas']):
+            print(f'{i+1}) {auternativa}')
+        resposta = int(input('Resposta: '))
+        if pergunta['resposta'] == pergunta['alternativas'][resposta-1]:
+            print('Resposta Certa')
+        else:
+            print('Resposta Errada')
+
+if cat == 1:
+    makingTheQuestion('general')
+elif cat == 2:
+    makingTheQuestion('history')
+elif cat == 3:
+    makingTheQuestion('science')
+elif cat == 4:
+    makingTheQuestion('geography')
+
