7066d689b39a60699049138495d8141a757996ad
Luiz-Ezequiel <112358317+Luiz-Ezequiel@users.noreply.github.com>
2024-04-18 11:52:54 -0300

    add timer of 10s for each question

--- perguntados.py
+++ perguntados.py
@@ -1,8 +1,20 @@
 import time
+import threading
 import random
 from categorias import categories, options
-from classes import *
+#from classes import *
 
+def get_user_input():
+    resposta = input('Resposta: ')
+    return resposta.strip()
+
+def timer(duration, user_input_event):
+    start_time = time.time()
+    end_time = start_time + duration
+    while time.time() < end_time:
+        if user_input_event.is_set():
+            return
+    print('Seu tempo acabou.')
 
 def makingAllQuestions(cat):
     for pergunta in categories[cat]:
@@ -15,25 +27,35 @@ def makingTheQuestion(pergunta):
     print()
     for i, alternativa in enumerate(pergunta['alternativas']):
         print(f'{i+1}) {alternativa}')
-    resposta = int(input('Resposta: '))
-    if resposta in [1, 2, 3, 4]:
-        if pergunta['resposta'] == pergunta['alternativas'][resposta-1]:
-            print('\nResposta Certa\n')
-            time.sleep(1)
-            return 1
+
+    user_input_event = threading.Event()
+    timer_thread = threading.Thread(target=timer, args=(10, user_input_event))
+    timer_thread.start()
+
+    resposta = get_user_input()
+    user_input_event.set()  # Signal to stop the timer thread
+
+    if resposta:
+        resposta = int(resposta)
+        if resposta in [1, 2, 3, 4]:
+            if pergunta['resposta'] == pergunta['alternativas'][resposta-1]:
+                print('\nResposta Certa\n')
+                time.sleep(1)
+                return 1
+            else:
+                print('\nResposta Errada\n')
+                time.sleep(1)
+                return 0
         else:
-            print('\nResposta Errada\n')
-            time.sleep(1)
-            return 0
-    else:
-        print('Opção tem que estar entre 1 e 4')
+            print('Opção tem que estar entre 1 e 4')
+    return 0
 
 # recebe a cópia como parametro para poder remover a pergunta após ser respondida evitando duplicatas
 def playGame(available_questions):
     life = 3
     i=0
     print('São 10 perguntas!')
-    print('Responda com cuidado!')
+    print('Você terá 10 segundos para responder cada pergunta')
     time.sleep(1)
     # Controla o fluxo do jogo, determina as vidas do jogador e itera sobre 10 perguntas aleatórias de diferentes categorias
     while i < 10:
